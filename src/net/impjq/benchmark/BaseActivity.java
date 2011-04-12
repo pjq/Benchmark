2b6d1073473057f25a9619c78f9902d55b5b9b8a
pjq <pengjianqing@gmail.com>
2011-04-12 11:11:17 +0800

    Add umeng

--- src/net/impjq/benchmark/BaseActivity.java
+++ src/net/impjq/benchmark/BaseActivity.java
@@ -1,6 +1,8 @@
 
 package net.impjq.benchmark;
 
+import com.mobclick.android.MobclickAgent;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -30,9 +32,19 @@ public class BaseActivity extends Activity {
     }
 
     @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
     protected void onPause() {
         // TODO Auto-generated method stub
         super.onPause();
+
+        MobclickAgent.onPause(this);
     }
 
     public void hideSoftKeyboard() {
